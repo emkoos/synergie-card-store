5ac1032b1d1c7df4148e7ab5b2482545bd4d95e2
Emil Saracyn <emil.saracyn@gmail.com>
2020-03-05 22:52:45 +0100

    Creating CardsEntities from DbContext

--- SynergieCardStore/Controllers/HomeController.cs
+++ SynergieCardStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SynergieCardStore.EF;
+using SynergieCardStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,17 @@ namespace SynergieCardStore.Controllers
 {
     public class HomeController : Controller
     {
+        private CardsEntities db = new CardsEntities();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+            Category category = new Category { CategoryName = "Książki i karty", CategoryDescription = "W kategorii znajdują się wszystkie książki i karty dotyczące tarota" };
+            db.Categories.Add(category);
+            db.SaveChanges();
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
---
+++ SynergieCardStore/EF/CardsEntities.cs
@@ -0,0 +1,32 @@
+﻿using SynergieCardStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SynergieCardStore.EF
+{
+    public class CardsEntities : DbContext
+    {
+        // Your context has been configured to use a 'CardsEntities' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'SynergieCardStore.EF.CardsEntities' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'CardsEntities' 
+        // connection string in the application configuration file.
+        public CardsEntities()
+            : base("name=SynergieStoreConnection")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<OrderPosition> OrderPosition { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+    }
+}
