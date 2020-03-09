9db1a107aa68a8e19856abe1b59bca178f0d47f4
Emil Saracyn <emil.saracyn@gmail.com>
2020-03-09 21:14:17 +0100

    Added register and login views

---
+++ SynergieCardStore/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using SynergieCardStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SynergieCardStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+        //GET
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SynergieCardStore/ViewModels/AccountViewModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SynergieCardStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić e-mail.")]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Musisz wprowadzić hasło.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} musi mieć co najmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź Hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
