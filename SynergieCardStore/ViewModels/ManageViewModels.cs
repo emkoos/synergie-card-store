4b753eb082700f20c6586b699caa8e57c112395d
Emil Saracyn <emil.saracyn@gmail.com>
2020-03-10 01:01:15 +0100

    Implementation of a partial user panel

---
+++ SynergieCardStore/ViewModels/ManageViewModels.cs
@@ -0,0 +1,35 @@
+﻿using SynergieCardStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SynergieCardStore.ViewModels
+{
+    public class ManageCredentialsViewModel
+    {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public SynergieCardStore.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public UserData UserData { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktualne hasło")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nowe hasło")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("NewPassword", ErrorMessage = "Hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
